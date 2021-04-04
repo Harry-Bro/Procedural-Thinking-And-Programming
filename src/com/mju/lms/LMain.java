571c82c2f4c7c19db35f137c378e490687bb9f99
Harry-Bro <likeharrybro@gmail.com>
2021-04-04 17:08:08 +0900

    feat: Add week5

--- src/com/mju/lms/LMain.java
+++ src/com/mju/lms/LMain.java
@@ -5,50 +5,62 @@ import java.io.IOException;
 public class LMain {
 
     public static void main(String[] args) throws IOException {
-        int result = charToInt();
-        char[] chars = intToChar(result);
-        for (int i = chars.length - 1; i >= 0; i--) {
-            if (chars[i] == 0)
-                continue;
-            System.out.printf("%s", chars[i]);
+        char[] inputArray = readInt();
+        int length = length(inputArray);
+        System.out.println("length: " + length);
+        int result = charToInt(inputArray);
+        System.out.println("charToInt Result: " + result);
+        char[] outputArray = intToChar(result, length);
+        System.out.print("inToChar Result: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(outputArray[i]);
         }
     }
 
-    public static int charToInt() throws IOException {
-        int[] inputArray = new int[10];
+    public static char[] readInt() throws IOException {
+        char[] inputArray = new char[10];
         int index = 0;
-        inputArray[index] = System.in.read();
+        inputArray[index] = (char) System.in.read();
         while (inputArray[index] >= '0' && inputArray[index] <= '9') {
             index++;
-            inputArray[index] = System.in.read();
+            inputArray[index] = (char) System.in.read();
         }
 
+        inputArray[index] = 0;
+        return inputArray;
+    }
+
+    public static int length(char[] inputArray) {
+        int length = 0;
+        while (inputArray[length] != 0)
+            length++;
+
+        return length;
+    }
+
+    public static int charToInt(char[] inputArray) throws IOException {
         int result = 0;
         int multiplier = 1;
-        for (int i = index - 1; i >= 0; i--) {
+        int length = length(inputArray);
+        for (int i = length - 1; i >= 0; i--) {
             result += (inputArray[i] - '0') * multiplier;
             multiplier *= 10;
         }
-//        System.out.println(result);
 
         return result;
     }
 
-    public static char[] intToChar(int result) {
-        int quotient = result;
-        int[] remainders = new int[10];
-        int index = 0;
-        while (quotient > 0) {
-            remainders[index] = quotient % 10;
-            quotient /= 10;
-            index++;
+    public static char[] intToChar(int result, int length) {
+        char[] output = new char[10];
+        int value = result;
+        int index = length - 1;
+        while (value > 0) {
+            output[index] = (char) (value % 10 + '0');
+            value /= 10;
+            index --;
         }
 
-        char[] charOutput = new char[10];
-        for (int i = 0; i < index; i++)
-            charOutput[i] = (char) (remainders[i] + '0');
-
-        return charOutput;
+        return output;
     }
 
 }
